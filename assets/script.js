bd478e5a46b382a861d9acf410b5c5025c4ec0eb
Nari-Al <153302709+Nari-Al@users.noreply.github.com>
2024-03-22 15:59:16 +0100

    Carousel
    Carousel intégré

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,68 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let carousel = document.querySelector(".banner-img");
+let i = 0;
+
+// Left Arrow
+
+let leftArrow = document.querySelector(".arrow_left");
+leftArrow.addEventListener("click", goLeft);
+function goLeft() {
+	console.log('go to left slide')
+	i--
+	if(i<0) {
+		i = slides.length-1
+	}
+	carousel.setAttribute("src", "assets/images/slideshow/" + slides[i].image)
+	let dotSelected = document.querySelector(".dot_selected")
+	if (dotSelected.previousElementSibling == null) {
+		dotContainer.lastElementChild.classList.add("dot_selected")
+		dotSelected.classList.remove("dot_selected")
+	}
+	else {
+		dotSelected.previousElementSibling.classList.add("dot_selected")
+		dotSelected.classList.remove("dot_selected")
+	}
+}
+
+// Right Arrow
+
+let rightArrow = document.querySelector(".arrow_right");
+rightArrow.addEventListener("click", goRight);
+function goRight() {
+	console.log('go to right slide')
+	i++
+	if (i==slides.length) {
+		i = 0
+	}
+	carousel.setAttribute("src", "assets/images/slideshow/" + slides[i].image)
+	let dotSelected = document.querySelector(".dot_selected")
+	if (dotSelected.nextElementSibling == null) {
+		dotContainer.firstElementChild.classList.add("dot_selected")
+		dotSelected.classList.remove("dot_selected")
+	}
+	else {
+		dotSelected.nextElementSibling.classList.add("dot_selected")
+		dotSelected.classList.remove("dot_selected")
+	}
+	
+}
+
+
+// i >= 0 && i <= 3
+
+// Dots
+
+let dotContainer = document.querySelector(".dots")
+
+// slides.forEach((slide) => console.log(slide)) 
+slides.forEach((slide) => {
+	const dot = document.createElement("div");
+	dot.className = "dot";
+	console.log(dot)
+	dotContainer.append(dot);
+})
+// first dot is selected by default
+dotContainer.firstElementChild.classList.add("dot_selected")
